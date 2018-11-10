1deff608f086ce5cf6d9f6016729658be3544e5d
Glitch (numerous-respect) <none>
2018-11-10 18:17:51 +0000

    Changed Activity and the help command

--- commands/help.js
+++ commands/help.js
@@ -2,9 +2,9 @@ module.exports = async (bot, message, args, Discord, moment) => {
     var embed = new Discord.RichEmbed()
         .setAuthor('Crash Commands')
         .setThumbnail(`${bot.user.avatarURL}`, false)
-        .addField(`${bot.config.prefix}hilfe`, "Zeigt diese Hilfe an mit allen Kommandos", false)
-        .addField(`${bot.config.prefix}anime [Anime Name]`, "Sucht auf Kitsu.io nach einem Anime und gibt 7 Resultate aus aus denen gewählt werden kann.", false)
-        .setFooter("Dab auf die Fehler.")
+        .addField(`${bot.config.prefix}help`, "Shows you this embed", false)
+        .addField(`${bot.config.prefix}anime [Anime Name]`, "Searches on Kitsu.io for the Anime Name and returns 7 results from which you can choose 1 and the Bot posts the Info as an embed in the channel.", false)
+        .setFooter("Dab on the errors.")
     		.setColor("#1919ff")
     await message.author.send(embed);
   	message.delete(1000)
