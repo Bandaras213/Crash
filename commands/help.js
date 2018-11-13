bf057ad630bd111515f01a1ebfb46c91374df6ab
Glitch (numerous-respect) <none>
2018-11-13 04:44:57 +0000

    updated help command

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,38 @@
 module.exports = async (bot, message, args, Discord, moment) => {
+let prefix = bot.config.prefix
 
-  let prefix = bot.config.prefix
-  var embed = new Discord.RichEmbed()
-    .setAuthor(`${bot.user.tag} Commands:`)
-    .setThumbnail(`${bot.user.avatarURL}`, false)
-    .addField(`${prefix}help`, "Shows you this embed", false)
-    .addField(`${prefix}anime [Anime Name]`, "Search on Kitsu.io for Anime.", false)
-    .addField(`${prefix}manga [Manga Name]`, "Search on Kitsu.io for Manga.", false)
-    .setFooter("Dab on the errors.")
-    .setColor("#1919ff");
-
-  await message.author.send(embed);
-  message.delete(1000);
+message.author.send({embed: {
+    color: "2123412",
+    author: {
+    	name: `${bot.user.tag}`,
+      icon_url: bot.user.avatarURL
+    },
+    title: "Help",
+    url: "http://github.com/Bandaras213/Crash/wiki/Commands",
+    description: "Thelse are all Commands with a description.",
+    fields: [{
+        name: `${prefix}help`, 
+        value: "Shows you this embed."
+      },
+      {
+        name: `${prefix}anime [Anime Name]`,
+        value: "Starts a search for a anime for you and let's you choose one of seven results."
+      },
+      {
+        name: `${prefix}manga [Manga Name]`,
+        value: "Starts a search for a Manga for you and let's you choose one of seven results."
+      }, 
+      {
+        name: `${prefix}rdmanime`,
+        value: "Searches a random anime for you."
+       }
+    ],
+    timestamp: new Date(),
+  	footer: {
+      icon_url: bot.user.avatarURL,
+      text: "Crash Bot coding at 4pm"
+    }
+  }
+                    } );
+message.delete(1000);
 };
