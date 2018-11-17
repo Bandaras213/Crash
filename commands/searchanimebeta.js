8c55fee112c75670f59d76dd1ab41e46b179818a
Glitch (numerous-respect) <none>
2018-11-17 18:30:15 +0000

    query command has its own file now

--- commands/searchanimebeta.js
+++ commands/searchanimebeta.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const query = require("../data/animequery.js");
 
 var emoji = [
     "❌", //X
@@ -27,90 +28,7 @@ module.exports = async (bot, message, args, Discord, moment) => {
         return message.channel.send(`${user}, I need a Title to search for! (Usage: €betaanime Title)`);
     };
 
-    var query = `
-	query ($id: Int, $page: Int, $perPage: Int, $search: String) {
-		Page (page: $page, perPage: $perPage) {
-    pageInfo {
-      total
-      currentPage
-      lastPage
-      hasNextPage
-      perPage
-}
-  media (id: $id, search: $search, type: ANIME) {
-    id
-    siteUrl
-		format
-    title {
-        romaji
-				english
-        }
-        coverImage {
-									large
-        }
-				bannerImage
-        status
-        description
-        averageScore
-				startDate {
-									day
-									month
-									year}
-				endDate {
-									day
-									month
-									year}
-				nextAiringEpisode{
-									id
-									airingAt
-									timeUntilAiring
-									episode}
-				season
-				episodes
-				duration
-				countryOfOrigin
-				isLicensed
-				source
-				trailer {
-								id
-								site}
-				genres
-				meanScore
-				popularity
-				isAdult
-				characters(page: 1, role: MAIN) {
-      	nodes {
-        id
-        name {
-          first
-          last
-        }
-				siteUrl
-      }
-    }
-				staff(page: 1) {
-				edges {
-      	node {
-        id
-        name {
-          first
-          last
-        }
-				siteUrl
-      	}
-				role
-			}
-    }
-				studios {
-      	nodes {
-        id
-        name
-      }
-		}
-}
-}
-}
-`;
+    await query
 
     let variables = {
         search: animename,
