4935e0119df5d416bc1c662367d4e353c8ae7c7b
Bandaras213 <you@example.com>
2019-12-14 04:01:52 +0100

    big upps

---
+++ data/activity.js
@@ -0,0 +1,37 @@
+module.exports = `
+query($userId: Int){
+  Page(perPage: 4) {
+    activities(userId: $userId, sort: ID_DESC, type: MEDIA_LIST) {
+      ... on ListActivity {
+        id
+        type
+        replyCount
+        status
+        progress
+        isLocked
+        likeCount
+        createdAt
+        user {
+          id
+          name
+          avatar {
+            large
+          }
+        }
+        media {
+          id
+          type
+          title {
+            romaji
+          }
+          episodes
+          siteUrl
+          coverImage {
+            large
+          }
+          bannerImage
+        }
+      }
+    }
+  }
+}`
